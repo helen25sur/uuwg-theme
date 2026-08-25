dd1f1afcbbc1dd21bb4d029d457a5bffbe46ee3d
Olena Surilova <elen21surilova@gmail.com>
2026-08-25 22:02:42 +0200

    fix: add adaptive styles for documents-grid

--- blocks/documents-grid/render.php
+++ blocks/documents-grid/render.php
@@ -22,8 +22,8 @@ $query = new WP_Query($args);
 
     <div class="uuwg-documents-grid__grid">
       <?php if ($query->have_posts()) : ?>
-        <?php while ($query->have_posts()) : ?>
-          <?php
+      <?php while ($query->have_posts()) : ?>
+      <?php
           $query->the_post();
           $file = function_exists('get_field')
             ? get_field('document_file')
@@ -36,35 +36,39 @@ $query = new WP_Query($args);
             : get_template_directory_uri() . '/assets/images/folder.svg';
           ?>
 
-          <div class="uuwg-documents-grid__card">
-            <div class="uuwg-documents-grid__card__img">
-              <img src="<?php echo esc_url($folder_icon_url); ?>" alt="Folder image">
-            </div>
-            <div class="uuwg-documents-grid__card__content">
-              <h3 class="uuwg-documents-grid__card__title">
-                <?php echo esc_html(get_the_title()); ?>
-              </h3>
-              <?php if (!is_wp_error($terms) && !empty($terms)) : ?>
-                <div class="uuwg-documents-grid__card__type">
-                  <?php foreach ($terms as $term) : ?>
-                    <span class="uuwg-documents-grid__card__type-name">
-                      <?php echo esc_html($term->name); ?>
-                    </span>
-                  <?php endforeach; ?>
-                </div>
-              <?php endif; ?>
-              <?php if ($file_url) : ?>
-                <a class="uuwg-documents-grid__card__button" href="<?php echo esc_url($file_url); ?>" download>
-                  <?php echo esc_html__('Download', 'uuwg'); ?>
-                </a>
-              <?php endif; ?>
-            </div>
+      <div class="uuwg-documents-grid__card">
+        <div class="uuwg-documents-grid__card__img">
+          <img src="<?php echo esc_url($folder_icon_url); ?>" alt="Folder image">
+        </div>
+        <div class="uuwg-documents-grid__card__content">
+          <h3 class="uuwg-documents-grid__card__title">
+            <?php echo esc_html(get_the_title()); ?>
+          </h3>
+          <?php if (!is_wp_error($terms) && !empty($terms)) : ?>
+          <div class="uuwg-documents-grid__card__type">
+            <?php foreach ($terms as $term) : ?>
+            <span class="uuwg-documents-grid__card__type-name">
+              <?php echo esc_html($term->name); ?>
+            </span>
+            <?php endforeach; ?>
           </div>
-        <?php endwhile; ?>
+          <?php endif; ?>
+          <?php if ($file_url) : ?>
+          <a class="uuwg-documents-grid__card__button" href="<?php echo esc_url($file_url); ?>" download>
+            <span class="uuwg-documents-grid__card__button__text">
+              <?php echo esc_html__('Download', 'uuwg'); ?>
+            </span>
+            <img src="<?php echo get_template_directory_uri() . '/assets/images/download.svg' ?>"
+              alt="icon of downloading">
+          </a>
+          <?php endif; ?>
+        </div>
+      </div>
+      <?php endwhile; ?>
       <?php else : ?>
-        <p class="uuwg-documents-grid__empty">
-          <?php echo esc_html__('No documents found.', 'uuwg'); ?>
-        </p>
+      <p class="uuwg-documents-grid__empty">
+        <?php echo esc_html__('No documents found.', 'uuwg'); ?>
+      </p>
       <?php endif; ?>
     </div>
   </div>
