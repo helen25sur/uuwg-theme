1df7c449802929a2f77b0de8b989185ec9c2aa27
Olena Surilova <elen21surilova@gmail.com>
2026-08-27 16:59:54 +0200

    refactor: change filter-documents.js

--- assets/js/filter-documents.js
+++ assets/js/filter-documents.js
@@ -3,7 +3,7 @@
 
   if (pageDocs) {
     const filterDocs = document.getElementById('uuwg-document-filter');
-    const filterItems = filterDocs.querySelectorAll('.uuwg-documents-filter__item');
+    const filterItems = filterDocs.querySelectorAll('.uuwg-documents-filter__item a');
 
     const documentGrid = pageDocs.querySelector('.uuwg-documents-grid__grid');
 
@@ -14,24 +14,34 @@
         filterItems.forEach(item => item.classList.remove('is-active'));
         f.classList.add('is-active');
 
-        const year = f.querySelector('a').innerText;
-        console.log(year);
+        const urlItem = new URL(f.href);
+        const year = urlItem.searchParams.get('document_year');
+
+        const startUrl = '/wp-json/uuwg/v1/document/';
         let url;
         const urlSite = new URL(window.location.href);
 
-        if (year === 'All') {
-          url = '/wp-json/uuwg/v1/document/';
+        if (year === null) {
+          url = startUrl;
           urlSite.searchParams.delete('document_year');
           window.history.pushState({}, '', urlSite);
         } else {
-          url = `/wp-json/uuwg/v1/document/?document_year=${year}`;
+          url = `${startUrl}?document_year=${encodeURIComponent(year)}`;
           urlSite.searchParams.set('document_year', year);
           window.history.pushState({}, '', urlSite);
         }
 
-        const response = await fetch(url);
-        const data = await response.json();
-        documentGrid.innerHTML = data.html;
+        try {
+          const response = await fetch(url);
+          if (!response.ok) {
+            throw new Error(`HTTP error: ${response.status}`);
+          }
+          const data = await response.json();
+          documentGrid.innerHTML = data.html;
+
+        } catch (error) {
+          console.error(error);
+        }
 
         filterDocs.removeAttribute('open');
 
