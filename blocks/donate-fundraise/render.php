9aab2878337ed40fbc71d9afcade0f3e1a08c91e
Olena Surilova <elen21surilova@gmail.com>
2026-09-02 14:51:02 +0200

    fix: take Donate URL from ACF Fields and Site Settings page into donate-fundraise block

--- blocks/donate-fundraise/render.php
+++ blocks/donate-fundraise/render.php
@@ -1,6 +1,16 @@
 <?php
 // Ensure $attributes is defined to avoid PHP notices when this template is rendered directly.
 $attributes = isset($attributes) && is_array($attributes) ? $attributes : (array) ($attributes ?? []);
+
+// If the button URL is not provided, we will try to get it from the site settings page (ACF Fields).
+$site_settings = get_page_by_path('site-settings');
+$donate_url = '';
+
+if (function_exists('get_field') && $site_settings) {
+  $donate_url = call_user_func('get_field', 'donate_url', $site_settings->ID);
+}
+
+$button_url = $attributes['buttonUrl'] ?: $donate_url;
 ?>
 
 <section <?php echo get_block_wrapper_attributes(['class' => 'uuwg-donate-fundraise alignfull']); ?>>
@@ -13,13 +23,13 @@ $attributes = isset($attributes) && is_array($attributes) ? $attributes : (array
 
     <div class="uuwg-donate-fundraise__grids">
       <?php for ($i = 1; $i <= 4; $i++) : ?>
-      <div class="uuwg-donate-fundraise__card">
-        <p class="uuwg-donate-fundraise__card__text"><?php echo esc_html($attributes["item{$i}Text"] ?? ''); ?></p>
-      </div>
+        <div class="uuwg-donate-fundraise__card">
+          <p class="uuwg-donate-fundraise__card__text"><?php echo esc_html($attributes["item{$i}Text"] ?? ''); ?></p>
+        </div>
       <?php endfor; ?>
     </div>
     <a class="uuwg-donate-fundraise__cta wp-element-button" target="_blank" rel="noreferrer noopener"
-      href="<?php echo esc_url($attributes['buttonUrl']); ?>" class="uuwg-btn">
+      href="<?php echo esc_url($button_url); ?>" class="uuwg-btn">
       <?php echo esc_html($attributes['buttonText']); ?>
     </a>
   </div>
