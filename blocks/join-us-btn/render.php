7e5978383037e94f5dd6b2c6817863e892d48a90
Olena Surilova <elen21surilova@gmail.com>
2026-09-09 13:50:23 +0200

    fix: edit block join-us-btn, move styles from header to own style file

--- blocks/join-us-btn/render.php
+++ blocks/join-us-btn/render.php
@@ -9,14 +9,8 @@ $linkTelegram = get_field('social_telegram', $settings_page->ID);
 
 ?>
 
-<!-- wp:buttons -->
-<div class="wp-block-buttons">
-  <!-- wp:button {"className":"is-style-outline chat_button__link"} -->
-  <div class="wp-block-button is-style-outline chat_button__link">
-    <a href="<?php echo $linkTelegram; ?>" target="_blank" rel="noopener noreferrer"
-      aria-label="Join Us: Telegram Channel" class="wp-block-button__link wp-element-button">Join our
-      chat</a>
-  </div>
-  <!-- /wp:button -->
+<div class="chat_button__container">
+  <a href="<?php echo $linkTelegram; ?>" target="_blank" rel="noopener noreferrer"
+    aria-label="Join Us: Telegram Channel" class="chat_button__link">Join our
+    chat</a>
 </div>
-<!-- /wp:buttons -->
